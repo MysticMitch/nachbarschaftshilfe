470e8c13df51d50b6ea9eba29a92d2eeec40e512
Mitch <muecahit.tuelek@gmail.com>
2021-08-23 08:12:05 +0200

    Profil angepasst

--- database/connection.js
+++ database/connection.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql2");
 const cred = require("./credentials.js");
+const {exec} = require("child_process");
 const daten = cred.daten; //Modul.Objekt
 
 const  connection = mysql.createConnection({
@@ -10,7 +11,10 @@ const  connection = mysql.createConnection({
   });
 
   connection.on("error", function(err) {
-    console.log("Verbindung zur Datenbank fehlgeschlagen.");
+    console.log("Datenbankverbindung fehlgeschlagen " + new Date);
+    exec("systemctl start 'mysql'");
+    exec("systemctl restart 'mysql'");
+    exec("pm2 restart 'server.js'");
   });
 
   module.exports.connection = connection;
